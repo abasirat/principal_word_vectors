0a00c9ff3b4bcab50dfa8fc6428186781121dca4
Ali Basirat <ali.basirat@lingfil.uu.se>
2019-03-18 16:32:32 +0100

    adding copyright header

--- pwvec/python/rand_pca.py
+++ pwvec/python/rand_pca.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+#
+#
+# This program is written by Ali Basirat ali.basirat@lingfil.uu.se as part of the 
+# project Principla Word Vectors at http://urn.kb.se/resolve?urn=urn:nbn:se:uu:diva-353866
+# You are allowed to modify or distribute it if you keep this header part
+# 
+#
+
 import pdb
 import numpy as np
 from scipy import linalg, sparse
